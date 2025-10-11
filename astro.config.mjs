4225e837f2ff55e642b2044a766ce8d32f1854e2
Enrique Albert <enalcadev@gmail.com>
2025-10-11 11:52:31 +0200

    Add initial Astro configuration with Tailwind CSS support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
+import path from 'node:path';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kilauea.es',
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  }
+});
